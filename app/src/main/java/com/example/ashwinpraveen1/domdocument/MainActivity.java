08d669987b727b76f0c8b6d52281560456d72955
Ashwin-Praveen <ashwinpraveen1@gmail.com>
2015-01-25 18:29:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashwinpraveen1/domdocument/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.ashwinpraveen1.domdocument;
+
+
+import org.xml.sax.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Document xmlDoc ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        xmlDoc = getDocument("./src/main/res/dom.xml");
+        System.out.println("Root:"+xmlDoc.getDocumentElement().getElementsByTagName("user1 "));
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+private static Document getDocument(String docs) {
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setIgnoringComments(true);
+            factory.setIgnoringElementContentWhitespace(true);
+            factory.setValidating(true);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            return builder.parse(new InputSource(docs));
+
+        }
+        catch(Exception e ) {
+
+        }
+
+        return null;
+    }
+
+}
